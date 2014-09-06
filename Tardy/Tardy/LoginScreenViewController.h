806f39bf07c4e7f60a0fc7062834ed9b841c4eda
apple <apple@587213239.guest.umich.net>
2014-09-06 06:19:09 -0400

    Storyboard, login screen, event tables, facebook API

---
+++ Tardy/Tardy/LoginScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginScreenViewController.h
+//  Tardy
+//
+//  Created by apple on 9/6/14.
+//  Copyright (c) 2014 Apple Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginScreenViewController : UIViewController
+
+@end
