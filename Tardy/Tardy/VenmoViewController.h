d1170a7340dbf6b5f227e26e7c61400dd08fc4fd
apple <apple@0587363172.wireless.umich.net>
2014-09-06 21:17:59 -0400

    able to get on venmo screen

---
+++ Tardy/Tardy/VenmoViewController.h
@@ -0,0 +1,19 @@
+//
+//  VenmoViewController.h
+//  Tardy
+//
+//  Created by apple on 9/6/14.
+//  Copyright (c) 2014 Apple Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenmoViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *toTextField;
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *noteTextField;
+@property (weak, nonatomic) IBOutlet UIButton *paybutton;
+
+
+
+@end
